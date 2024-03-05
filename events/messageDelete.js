01072384d06f869989cf7ff6378ee03d8ea9c0ea
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-05 19:12:34 +0900

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,15 +4,30 @@ const path = require('path');
 module.exports = {
     name: 'messageDelete',
     execute(message) {
-        console.log(`Message deleted in ${message.channel.name}: ${message.content} by ${message.author.tag}`);
+        if (message.author.bot) return;
 
-        const historyFilePath = path.join(__dirname, `../history/${message.channel.id}history.txt`);
-        const logMessage = `${message.author.tag}: ${message.content}\n`;
+        console.log(`Message deleted in #${message.channel.name || 'DM'}: "${message.content}" by ${message.author.tag}`);
 
-        fs.appendFile(historyFilePath, logMessage, (err) => {
+        const historyFilePath = path.join(__dirname, '..', 'history', `${message.channel.id}history.txt`);
+
+        fs.readFile(historyFilePath, 'utf8', (err, data) => {
             if (err) {
-                console.error(`Error writing to history file: ${err}`);
+                console.error(err);
+                return;
             }
+
+            const historyContent = JSON.parse(data || '{}');
+
+            historyContent[message.id] = {
+                author: message.author.tag,
+                content: message.content,
+            };
+
+            fs.writeFile(historyFilePath, JSON.stringify(historyContent, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
         });
     },
-}
+};
