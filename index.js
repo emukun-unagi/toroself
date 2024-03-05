e715e3b653bb187fd7de9fa4000fafc40c230bda
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-05 17:21:16 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js-selfbot-v13');
+const fetch = require('node-fetch');
+const chalk = require('chalk');
+const fs = require('fs');
+require('dotenv').config();
+
+const client = new Discord.Client({
+    checkUpdate: false,
+});
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.login(process.env.token);
