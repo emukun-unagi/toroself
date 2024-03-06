c6db15383f02959c2e608f4b365228738cf9b065
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-06 14:56:11 +0900

    Create miq.js

---
+++ events/miq.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Discord = require('discord.js-selfbot-v13');
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'miq',
+    description: 'Send specified user information and message to miq-api',
+    async execute(message, args) {
+        if (message.author.bot) return;
+
+        if (args.length < 2) {
+            return message.channel.send('Please provide the user mention, ID, or tag, and the message content.');
+        }
+
+        const userIdentifier = args[0];
+        const content = args.slice(1).join(' ');
+
+        let user;
+        
+        if (userIdentifier.startsWith('<@') && userIdentifier.endsWith('>')) {
+            user = message.mentions.users.first() || await message.client.users.fetch(userIdentifier.slice(2, -1)).catch(() => null);
+        } else {
+            user = await message.client.users.fetch(userIdentifier).catch(() => null);
+        }
+
+        if (!user) {
+            return message.channel.send('Unable to find the specified user.');
+        }
+
+        const miqApiUrl = `https://miq-api.onrender.com/?type=color&name=${encodeURIComponent(user.username)}&id=${encodeURIComponent(user.tag)}&icon=${encodeURIComponent(user.displayAvatarURL({ format: 'png' }))}&content=${encodeURIComponent(content)}`;
+
+        message.channel.send(miqApiUrl);
+    },
+};
