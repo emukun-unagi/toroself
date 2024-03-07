6987deb1c5a5ca629c3a16003387a2a4c5f5f06c
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-07 17:58:06 +0900

    Update miq2.js

--- commands/miq2.js
+++ commands/miq2.js
@@ -4,32 +4,21 @@ const fetch = require('node-fetch');
 
 module.exports = {
     name: 'miq2',
-    description: 'Send specified user information and message to miq-api for +miq2',
+    description: 'Send specified user and message information to miq-api',
     async execute(message, args) {
         if (message.author.bot) return;
 
-        if (args.length < 2) {
-            return message.channel.send('Please provide the user mention, ID, or tag, and the message content.');
+        const replyMessage = message.reference?.messageID
+            ? await message.channel.messages.fetch(message.reference.messageID).catch(() => null)
+            : null;
+
+        if (!replyMessage) {
+            return message.channel.send('Please reply to a message using +miq2.');
         }
 
-        const userIdentifier = args[0];
-        const content = args.slice(1).join(' ');
-
-        let user;
-
-        if (userIdentifier.startsWith('<@') && userIdentifier.endsWith('>')) {
-            user = message.mentions.users.first() || await message.client.users.fetch(userIdentifier.slice(2, -1)).catch(() => null);
-        } else {
-            user = await message.client.users.fetch(userIdentifier).catch(() => null);
-        }
-
-        if (!user) {
-            return message.channel.send('Unable to find the specified user.');
-        }
-
+        const user = replyMessage.author;
         const name = user.nickname || user.username;
-
-        const miqApiUrl = `https://miq-api.onrender.com/?type=color&name=${encodeURIComponent(name)}&id=${encodeURIComponent(user.tag)}&icon=${encodeURIComponent(user.displayAvatarURL({ format: 'png' }))}&content=${encodeURIComponent(content)}`;
+        const miqApiUrl = `https://miq-api.onrender.com/?type=color&name=${encodeURIComponent(name)}&id=${encodeURIComponent(user.tag)}&icon=${encodeURIComponent(user.displayAvatarURL({ format: 'png' }))}&content=${encodeURIComponent(replyMessage.content)}`;
 
         message.channel.send(miqApiUrl);
     },
