18ecd863a164452bf658ca51576181395b83b388
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-07 18:06:38 +0900

    Update miq2.js

--- commands/miq2.js
+++ commands/miq2.js
@@ -1,23 +1,16 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const Discord = require('discord.js-selfbot-v13');
 
 module.exports = {
     name: 'miq2',
-    description: 'Send specified user and message information to miq-api',
+    description: 'Send specified user information and message to miq-api (based on reply)',
     async execute(message) {
-        if (message.author.bot) return;
+        if (message.author.bot || !message.reference) return;
 
-        const replyMessage = message.reference?.messageID
-            ? await message.channel.messages.fetch(message.reference.messageID).catch(() => null)
-            : null;
-
-        if (!replyMessage) {
-            return message.channel.send('Please reply to a message using +miq2.');
-        }
-
-        const user = replyMessage.author;
+        const repliedMessage = await message.channel.messages.fetch(message.reference.messageID);
+        const user = repliedMessage.author;
         const name = user.nickname || user.username;
-        const miqApiUrl = `https://miq-api.onrender.com/?type=color&name=${encodeURIComponent(name)}&id=${encodeURIComponent(user.tag)}&icon=${encodeURIComponent(user.displayAvatarURL({ format: 'png' }))}&content=${encodeURIComponent(replyMessage.content)}`;
+
+        const miqApiUrl = `https://miq-api.onrender.com/?type=color&name=${encodeURIComponent(name)}&id=${encodeURIComponent(user.tag)}&icon=${encodeURIComponent(user.displayAvatarURL({ format: 'png' }))}&content=${encodeURIComponent(repliedMessage.content)}`;
 
         message.channel.send(miqApiUrl);
     },
