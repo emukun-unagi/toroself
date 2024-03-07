4d0be6c5b6a4cdd8b1cf69a4849c6ebf01a8882c
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-07 18:14:57 +0900

    Update miq2.js

--- commands/miq2.js
+++ commands/miq2.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js-selfbot-v13');
 
 module.exports = {
     name: 'miq2',
-    description: 'Send specified user information and message to miq-api (based on reply)',
+    description: 'Send information about the user and replied message to miq-api',
     async execute(message) {
         if (message.author.bot || !message.reference) return;
 
         const repliedMessage = await message.channel.messages.fetch(message.reference.messageID);
-        const user = repliedMessage.author;
+        const user = message.author;
         const name = user.nickname || user.username;
 
         const miqApiUrl = `https://miq-api.onrender.com/?type=color&name=${encodeURIComponent(name)}&id=${encodeURIComponent(user.tag)}&icon=${encodeURIComponent(user.displayAvatarURL({ format: 'png' }))}&content=${encodeURIComponent(repliedMessage.content)}`;
