6357e0079bfea9069b211385a341c38975698f3c
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-08 15:48:03 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const config = require('../config.json');
+const config = require('./config.json');
 
 module.exports = {
     name: 'ready',
@@ -12,30 +12,7 @@ module.exports = {
             status: config.status.presence
         };
 
-        const button = {
-            type: 2, // TYPE: 2 is a BUTTON with URL
-            style: 1,
-            label: 'とろろ',
-            url: 'https://example.com', // ここに移動するURLを指定
-        };
-
         client.user.setPresence(status);
-
-        client.api.applications(client.user.id).guilds(config.guildId).commands.post({
-            data: {
-                name: 'profile',
-                description: 'とろろのプロフィールを表示します。',
-                options: [],
-            },
-        });
-
-        client.on('interactionCreate', async (interaction) => {
-            if (!interaction.isButton()) return;
-
-            if (interaction.customId === 'profile_button') {
-                // ボタンが押されたとき、URLに移動するのでここでは何も返信しない
-            }
-        });
 
         console.log(`${client.user.tag} is online!`);
     },
