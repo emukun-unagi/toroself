e1e7c5c95d39ec69707d7c96ad6359b57b2f53bd
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-08 15:06:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const chalk = require('chalk');
 const fs = require('fs');
 const config = require('./config.json');
 require('dotenv').config();
+const express = require(`express`)
+const app = express();
 
 const client = new Discord.Client({
     checkUpdate: false,
@@ -16,42 +18,37 @@ http.createServer(function(request, response)
 	response.end('Bot is online!');
 }).listen(3000);
 
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.on('message', async (message) => {
+  if(message.author.bot)return;
+  if(message.content.includes(client.user.id)) {
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
+    let lol = new MessageEmbed()
+    .setColor("#00FFFF")
+    .setAuthor(client.user.username, message.author.displayAvatarURL({dynamic: true}))
+    .setDescription(`My prefix is **+** To get started type \`+help\``)
+    message.channel.send(1214791259147862057)
+  } 
+});
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+fs.readdir(__dirname + "/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    console.log("Loading Event: "+eventName)
+  });
+});
 
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, client));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args, client));
-    }
-}
-
-const prefix = config.prefix;
-
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (!client.commands.has(command)) return;
-
-    try {
-        client.commands.get(command).execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('An error occurred while executing the command.');
-    }
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
+  });
 });
 
 client.login(process.env.token);
