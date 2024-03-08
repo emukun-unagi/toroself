74fcd7ac4c8e51f896321b5e72b09ea29869ca1e
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-08 15:55:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
-    console.log(chalk.green(`Loaded Command: ${file}`)); // Log the loaded command
+    console.log(chalk.green(`Loaded Command: ${file}`));
 }
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
@@ -29,15 +29,9 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
     if (event.once) {
-        client.once(event.name, (...args) => {
-            event.execute(...args, client);
-            console.log(chalk.blue(`Loaded Event: ${file}`)); // Log the loaded event
-        });
+        client.once(event.name, (...args) => event.execute(...args, client));
     } else {
-        client.on(event.name, (...args) => {
-            event.execute(...args, client);
-            console.log(chalk.blue(`Loaded Event: ${file}`)); // Log the loaded event
-        });
+        client.on(event.name, (...args) => event.execute(...args, client));
     }
 }
 
