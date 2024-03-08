92877e2481a0002ff6a0631bf23e8acffa8dcad0
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-08 14:55:59 +0900

    Update message.js

--- events/message.js
+++ events/message.js
@@ -25,5 +25,24 @@ module.exports = {
             console.log(`Channel: ${message.channel.name} (ID: ${message.channel.id})`);
             console.log('------------------------------');
         }
+
+        const prefixMention = new RegExp(`^<@!?${message.client.user.id}> `);
+        const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : message.client.config.prefix;
+
+        if (message.content.indexOf(prefix) !== 0) return;
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+        const cmd = message.client.commands.get(command);
+        const aliases = message.client.commands.find(x => x.info.aliases.includes(command));
+
+        if (cmd) {
+            cmd.run(message.client, message, args);
+        } else if (aliases) {
+            aliases.run(message.client, message, args);
+        } else {
+            return;
+        }
     },
 };
