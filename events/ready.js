4d757fbb491b93ddf699dca88e8bd18eb02c7b43
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-08 15:36:19 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,17 @@
+const config = require('./config.json');
+
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        console.log(`${client.user.tag} is online!`)
+        const status = {
+            activities: [{
+                name: config.status.text,
+                type: config.status.type
+            }],
+            status: config.status.presence
+        };
+        client.user.setPresence(status);
+        console.log(`${client.user.tag} is online!`);
     },
 };
