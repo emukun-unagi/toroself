c16ffbe2184c7ea7533fc9d318e95659ea223e8a
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-08 15:19:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const chalk = require('chalk');
 const fs = require('fs');
 const config = require('./config.json');
 require('dotenv').config();
-const express = require(`express`)
-const app = express();
 
 const client = new Discord.Client({
     checkUpdate: false,
@@ -17,18 +15,6 @@ http.createServer(function(request, response)
       response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Bot is online!');
 }).listen(3000);
-
-client.on('message', async (message) => {
-  if(message.author.bot)return;
-  if(message.content.includes(client.user.id)) {
-
-    let lol = new MessageEmbed()
-    .setColor("#00FFFF")
-    .setAuthor(client.user.username, message.author.displayAvatarURL({dynamic: true}))
-    .setDescription(`My prefix is **+** To get started type \`+help\``)
-    message.channel.send(1214791259147862057)
-  } 
-});
 
 fs.readdir(__dirname + "/events/", (err, files) => {
   if (err) return console.error(err);
