6c3c3f9cbfa8173f90b9c905452dfc5b889826f4
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-09 13:44:51 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args) {
         message.reply('Pinging...').then(sentMessage => {
             const ping = sentMessage.createdTimestamp - message.createdTimestamp;
-            sentMessage.edit(`Pong! Latency is ${ping}ms.`);
+            sentMessage.edit(`${ping}ms`);
         });
     },
 };
