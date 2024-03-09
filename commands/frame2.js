25502c6b424aa6ff569e6e1d8f0b3910a5d78971
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-09 13:00:53 +0900

    Update frame2.js

--- commands/frame2.js
+++ commands/frame2.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'frame2 command',
     async execute(message, args) {
         if (args.length !== 1) {
-            return message.reply(`Usage: \`${prefix}frame1 <user_id | user_mention | user_tag>\``);
+            return message.reply(`ユーザーのメンション、ID、タグを入力してください。`);
         }
 
         const userIdentifier = args[0];
