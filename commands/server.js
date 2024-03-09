d841dcc7c43a99d0a985ce8b65a74747f72c1e21
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-09 14:20:13 +0900

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'server',
     description: 'Display server information',
     execute(message) {
-        const server = message.guild;
-        if (!server) {
+        if (!message.guild) {
             return message.channel.send('This command can only be used in a server.');
         }
 
+        const server = message.guild;
         const owner = server.owner;
         const memberCount = server.memberCount;
         const channelCount = server.channels.cache.size;
@@ -14,13 +14,13 @@ module.exports = {
         const boostLevel = server.premiumTier;
         const boostCount = server.premiumSubscriptionCount;
 
-        const serverInfo = `**サーバー情報:**\n\`\`\`\n` +
-            `サーバー: ${server.name} (${server.id})\n` +
-            `オーナー: ${owner.user.tag} (${owner.id})\n` +
-            `メンバー: ${memberCount}\n` +
-            `チャンネル: ${channelCount}\n` +
-            `ロール: ${roleCount}\n` +
-            `ブースト: ${boostLevel} (${boostCount} boosts)\n` +
+        const serverInfo = `**Server Information**\n\`\`\`\n` +
+            `Server Name: ${server.name} (${server.id})\n` +
+            `Owner: ${owner ? `${owner.user.tag} (${owner.id})` : 'N/A'}\n` +
+            `Members: ${memberCount}\n` +
+            `Channels: ${channelCount}\n` +
+            `Roles: ${roleCount}\n` +
+            `Boost Level: ${boostLevel} (${boostCount} boosts)\n` +
             `\`\`\``;
 
         message.channel.send(serverInfo);
