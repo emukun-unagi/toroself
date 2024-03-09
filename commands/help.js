396dc8b11124abb946d5fef32f4a0a38ce83a0fa
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-09 13:55:30 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 const { prefix } = require('../config.json');
+
+const loadCommands = (commandsDir) => {
+    const commands = [];
+
+    const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(path.join(commandsDir, file));
+        commands.push(command);
+    }
+
+    return commands;
+};
+
+const commandsDir = path.resolve(__dirname, 'commands');
+const commands = loadCommands(commandsDir);
 
 module.exports = {
     name: 'help',
-    description: 'help command',
-    execute(message, args, commands) {
+    description: 'ヘルプコマンド',
+    execute(message, args) {
         const commandList = commands.map(command => `**${prefix}${command.name}**: ${command.description}`).join('\n');
         const helpMessage = `**コマンド一覧:**\n${commandList}`;
         message.reply(helpMessage);
