109ee420de52c7f0c1507ee9cbcc75c896e9eafd
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-09 14:06:41 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,29 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const { prefix } = require('../config.json');
-
-const loadCommands = (commandsDir) => {
-    const commands = [];
-
-    const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-        const command = require(path.join(commandsDir, file));
-        commands.push(command);
-    }
-
-    return commands;
-};
-
-const commandsDir = path.resolve(__dirname, 'commands');
-const commands = loadCommands(commandsDir);
+const { prefix } = require('../config.json'); // Make sure to adjust the path if needed
 
 module.exports = {
     name: 'help',
-    description: 'ヘルプコマンド',
-    execute(message, args) {
-        const commandList = commands.map(command => `**${prefix}${command.name}**: ${command.description}`).join('\n');
-        const helpMessage = `**コマンド一覧:**\n${commandList}`;
-        message.reply(helpMessage);
+    description: 'Display available commands',
+    execute(message, args, commands) {
+        const helpMessage = `**コマンド一覧:**\n\`\`\`\nBot: help,ping\nAi: gpt\nFun: miq\nOther: snipe\n\`\`\``;
+        message.channel.send(helpMessage);
     },
 };
