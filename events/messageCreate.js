75a80c665628b05425ff01dbf7c5f5f1655b8a87
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-10 09:55:41 +0900

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,19 @@
+import config from '../config.js';
+
+export default async (client, message) => {
+    if (!message.content.startsWith(config.prefix) || message.author.id === client.user.id) return;
+
+    if (config.allowed.length !== 0 && !config.allowed.includes(message.author.id)) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const commandData = client.commands.get(command);
+    if (!commandData) return;
+
+    try {
+        await commandData.execute(client, message, args);
+    } catch (error) {
+        console.error(`Error executing command '${command}': ${error}`);
+    }
+};
