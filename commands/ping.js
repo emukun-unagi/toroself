873e40e794433d596874f487f9b3207870ff6623
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-11 17:24:07 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('./config.json');
+const config = require('../config.json');
 
 module.exports = {
     name: 'ping',
