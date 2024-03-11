d00829d0865e6f97c8cb9971344ff7e79de3f0be
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-11 17:04:20 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('./config.json');
+
 module.exports = {
     name: 'ping',
     description: 'ping command',
     execute(message, args) {
-        message.reply('Pinging...').then(sentMessage => {
-            const ping = sentMessage.createdTimestamp - message.createdTimestamp;
-            sentMessage.edit(`${ping}ms`);
-        });
+        const whitelistPath = path.join(__dirname, 'whitelist.json');
+        const whitelist = JSON.parse(fs.readFileSync(whitelistPath, 'utf8'));
+        const userID = message.author.id;
+        if (whitelist.allowedUsers.includes(userID) || userID === config.owner) {
+            message.reply('Pinging...').then(sentMessage => {
+                const ping = sentMessage.createdTimestamp - message.createdTimestamp;
+                sentMessage.edit(`${ping}ms`);
+            });
+        }
     },
 };
