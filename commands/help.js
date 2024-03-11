6e0b78ee852cf7e0099f67e9eb6b150f763510c9
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-11 17:58:08 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,28 @@
-const { prefix } = require('../config.json'); // Make sure to adjust the path if needed
+const { prefix } = require('../config.json');
+const fs = require('fs');
+const path = require('path');
+const config = require('../config.json');
 
 module.exports = {
     name: 'help',
     description: 'Display available commands',
     execute(message, args, commands) {
+        const userID = message.author.id;
+
+        const whitelistPath = path.join(__dirname, '../whitelist.json');
+
+        const whitelist = JSON.parse(fs.readFileSync(whitelistPath, 'utf8'));
+
+        if (!whitelist.allowedUsers.includes(userID) && userID !== config.owner) {
+            return message.reply('このコマンドを使用する権限がありません。');
+        }
+
         const helpMessage = `**コマンド一覧:**\n\`\`\`\n` +
             `BOT: help, ping\n` +
             `AI: gpt\n` +
             `情報: server\n` +
             `楽しい: snipe, miq\n` +
+            `オーナー: addition, delete\n` +
             `\`\`\``;
         message.reply(helpMessage);
     },
