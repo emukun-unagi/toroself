eaf4b51c1bf306dab7b9f8863eafd566c804f6d2
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-11 18:08:53 +0900

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -14,7 +14,7 @@ module.exports = {
         const whitelist = JSON.parse(fs.readFileSync(whitelistPath, 'utf8'));
 
         if (!whitelist.allowedUsers.includes(userID) && userID !== config.owner) {
-            return message.reply('このコマンドを使用する権限がありません。');
+            return;
         }
 
         if (!message.guild) {
