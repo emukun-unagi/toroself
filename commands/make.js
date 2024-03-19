097b0994efc0da4647fa7a215e0c2ee3f91fc1d2
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-19 12:57:06 +0900

    Update make.js

--- commands/make.js
+++ commands/make.js
@@ -16,22 +16,15 @@ module.exports = {
             const avatarURL = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`;
 
             const content = args.join(' ');
-            const embed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Make Command')
-                .setAuthor(user.tag, avatarURL)
-                .setDescription(content);
-
             const avatarLink = encodeURIComponent(avatarURL);
             const name = encodeURIComponent(user.username);
             const tag = encodeURIComponent(user.tag);
             const contentEncoded = encodeURIComponent(content);
             const apiUrl = `https://miq-api.onrender.com/type=color&name=${name}&id=${tag}&icon=${avatarLink}&content=${contentEncoded}`;
 
-            message.channel.send(embed)
-                .then(() => {
-                    message.channel.send(apiUrl);
-                })
+            const replyMessage = `User: ${user.tag}\nAvatar: ${avatarURL}\nContent: ${content}\nAPI URL: ${apiUrl}`;
+
+            message.channel.send(replyMessage)
                 .catch(err => {
                     console.error('Error sending message:', err);
                 });
