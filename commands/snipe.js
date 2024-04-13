ce9975db1b76d4f1e8afcf0dba25f443e51d5eab
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-04-13 18:24:32 +0900

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -3,39 +3,63 @@ const path = require('path');
 const config = require('../config.json');
 const whitelistPath = path.join(__dirname, '../whitelist.json');
 
-
-
+const HISTORY_PATH = './history';
 
 module.exports = {
   name: 'snipe',
   description: 'snipe command',
   execute(message, args) {
-    const channelId = message.channel.id;
-    const historyPath = path.join(__dirname, `../history/${channelId}.txt`);
+    const whitelist = JSON.parse(fs.readFileSync(whitelistPath, 'utf8'));
+    const userID = message.author.id;
+
+    if (!fs.existsSync(HISTORY_PATH)) {
+      fs.mkdirSync(HISTORY_PATH);
+    }
+
+    const historyPath = path.join(HISTORY_PATH, `${message.channel.id}.txt`);
 
     if (!fs.existsSync(historyPath)) {
-      message.reply('No message history found.');
-      return;
+      fs.writeFileSync(historyPath, '');
     }
 
-    const history = fs.readFileSync(historyPath, 'utf8');
-    const messages = history.split('--end--');
+    const history = fs.readFileSync(historyPath, 'utf8').split('-----\n');
+    const lines = history.map(line => line.split('\n').filter(Boolean)).filter(line => line.length > 0);
 
-    if (args.length < 1) {
-      args = [messages.length - 1];
-    } else if (isNaN(args[0]) || parseInt(args[0]) < 1 || parseInt(args[0]) > messages.length) {
-      message.reply('Invalid message number.');
-      return;
-    }
+    if (whitelist.allowedUsers.includes(userID) || userID === config.owner) {
+      if (args.length < 1) {
+        message.reply('Usage: +snipe [number]');
+        return;
+      }
 
-    const messageIndex = parseInt(args[0]) - 1;
-    const messageText = messages[messageIndex].split('--author--')[1];
-    const messageTime = messages[messageIndex].split('--author--')[0];
+      const num = Number(args[0]);
 
-    if (messageText.startsWith('deleted by ')) {
-      message.channel.send(`${messageText.slice(11)}\n---\n${messageTime}`);
-    } else {
-      message.channel.send(`${messageText}\n---\n${messageTime}`);
+      if (isNaN(num)) {
+        message.reply('Invalid number');
+        return;
+      }
+
+      if (num <= 0 || num > lines.length) {
+        message.reply(`Number must be between 1 and ${lines.length}`);
+        return;
+      }
+
+      const targetLines = lines[lines.length - num];
+
+      const reply = targetLines
+        .map((line, i) => {
+          if (line.startsWith('deleted by ')) {
+            const index = line.indexOf(': ');
+            return `deleted by ${line.slice(9, index)}: ${line.slice(index + 2)}`;
+          } else if (line.startsWith('edited by ')) {
+            const index = line.indexOf(': ');
+            return `edited by ${line.slice(8, index)}: ${line.slice(index + 2)}`;
+          } else {
+            return line;
+          }
+        })
+        .join('\n');
+
+      message.reply(reply);
     }
   },
 };
